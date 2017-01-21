fae0125d221f428ea0af1a16a0860e70ed0a7556
Neng Liangpornrattana <punneng@gmail.com>
2017-01-22 03:54:50 +0700

    Add test case when ScraperService.scrape couldnt find a matching

--- test/services/scraper-spec.js
+++ test/services/scraper-spec.js
@@ -8,41 +8,59 @@ const Const          = require('../../models/constant')
 const ScraperService = require('../../services/scraper-service')
 
 describe('ScraperService', () => {
-  describe('scrape()', () => {
-    let sandbox
-    let requestStub
-    let regexpSpy
-    beforeEach(() => {
-      sandbox = Sinon.sandbox.create()
-      requestStub = sandbox.stub(Request, 'get').withArgs('http://www.nasdaq.com/').returns(
-        P.resolve(String(fs.readFileSync(__dirname + '/fixtures/nasdaq.html', 'utf-8')))
-      )
-      regexpSpy = sandbox.spy(Const.INDEX_REGEXP, 'exec')
-    })
+  describe('#scrape()', () => {
+    describe('executes successfully', () => {
+      let sandbox
+      let requestStub
+      let regexpSpy
+      beforeEach(() => {
+        sandbox = Sinon.sandbox.create()
+        requestStub = sandbox.stub(Request, 'get').withArgs('http://www.nasdaq.com/').returns(
+          P.resolve(String(fs.readFileSync(__dirname + '/fixtures/nasdaq.html', 'utf-8')))
+        )
+        regexpSpy = sandbox.spy(Const.INDEX_REGEXP, 'exec')
+      })
 
-    afterEach(() => {
-      sandbox.restore()
-    })
-    it('should be able to connect to http://www.nasdaq.com/', () => {
-      return ScraperService.scrape()
-      .then(() => {
-        assert(requestStub.calledOnce)
+      afterEach(() => {
+        sandbox.restore()
+      })
+      it('should be able to connect to http://www.nasdaq.com/', () => {
+        return ScraperService.scrape()
+        .then(() => {
+          assert(requestStub.calledOnce)
+        })
+      })
+
+      it('should match value, change net and change net precentage only on nasdaq', () => {
+        return ScraperService.scrape()
+        .then(res => {
+          assert(regexpSpy.calledOnce)
+        })
+      })
+
+      it('should return an object of value, change net and change net precentage only on nasdaq', () => {
+        return ScraperService.scrape()
+        .then(res => {
+          assert.strictEqual(res.value, 5555.33)
+          assert.strictEqual(res.changeNet, 15.25)
+          assert.strictEqual(res.changeNetPercent, 0.28)
+        })
       })
     })
 
-    it('should match value, change net and change net precentage only on nasdaq', () => {
-      return ScraperService.scrape()
-      .then(res => {
-        assert(regexpSpy.calledOnce)
+    describe('executes unsuccessfuly', () => {
+      beforeEach(() => {
+        sandbox = Sinon.sandbox.create()
+        requestStub = sandbox.stub(Request, 'get').withArgs('http://www.nasdaq.com/').returns(
+          P.resolve('failed')
+        )
       })
-    })
 
-    it('should return an object of value, change net and change net precentage only on nasdaq', () => {
-      return ScraperService.scrape()
-      .then(res => {
-        assert.strictEqual(res.value, 5555.33)
-        assert.strictEqual(res.changeNet, 15.25)
-        assert.strictEqual(res.changeNetPercent, 0.28)
+      it('should raise TypeError exception', () => {
+        return ScraperService.scrape()
+        .catch(e => {
+          assert.equal(e.name, 'TypeError')
+        })
       })
     })
   })
