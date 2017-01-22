8f6bb4333542c907c4068f62b5c266975a758023
Neng Liangpornrattana <punneng@gmail.com>
2017-01-23 03:18:40 +0700

    Fix typo

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const cors = require('cors')
 
-const Clinet           = require('./client')
+const Client           = require('./client')
 const StockIndexes     = require('./rest/stock-indexes')
 const ScraperScheduler = require('./schedule/scraper-scheduler')
 
 app = express()
 app.use(cors())
 
-Clinet.register(app)
+Client.register(app)
 StockIndexes.register(app)
 app.listen(8080, () => {
     console.log('Server listen on 8080')
